ef11d4a27b633e77b755b6ea32e2b3bb57340eac
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-24 02:31:50 +0100

    Q08 - initial commit - different local variables added to see the affect on max no. of function calls before StackOverflow

---
+++ src/Q08.groovy
@@ -0,0 +1,39 @@
+/**
+ * @author crkimberley on 24/08/2016.
+ */
+stackSize(1)
+
+void stackSize(int n) {
+    int a = 1
+    int b = 2
+    int c = 3
+    int d = 4
+    int e = 5
+    int f = 6
+    int g = 7
+    int h = 8
+    int i = 9
+    int j = 10
+    double aa = 1
+    double bb = 2
+    double cc = 3
+    double dd = 4
+    double ee = 5
+    double ff = 6
+    double gg = 7
+    double hh = 8
+    double ii = 9
+    double jj = 10
+    String string0 = "" + n
+    String string1 = "1"
+    String string2 = "2"
+    String string3 = "3"
+    String string4 = "4"
+    String string5 = "5"
+    String string6 = "6"
+    String string7 = "7"
+    String string8 = "8"
+    String string9 = "9"
+    println "Method call " + string0
+    stackSize(n + 1)
+}
